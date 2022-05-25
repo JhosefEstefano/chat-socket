aac897bdaab6ef5ed340f501347e52ab1237c7ed
JhosefGrey <jhosefgrey@gmail.com>
2022-05-25 10:48:35 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const port = process.env.PORT || 3000;
+const io = new Server(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("usuario invalido"));
+  }
+
+  socket.username = username;
+  next();
+});
+
+io.on("connection", (socket) => {
+  const users = [];
+  const id_socket = socket.id;
+  let username;
+
+  for (let [id, socket] of io.of("/").sockets) {
+    if (socket.username === "central")
+      users.push({
+        userID: id,
+        username: socket.username,
+      });
+
+    if (socket.id === id_socket) {
+      username = socket.username;
+      users.push({
+        userID: id,
+        username: socket.username,
+      });
+    }
+  }
+
+  io.emit("users", users);
+
+  socket.emit("receive", {
+    message: `Bienvenido, ${username}`,
+    userID: id_socket,
+    username: "Sistema",
+  });
+
+  socket.on("send", ({ content, to }) => {
+    socket.to(to).emit("receive", content);
+  });
+
+  socket.on("terminate", (to) => {
+    socket.to(to).emit("re-catch-terminate");
+  });
+
+  socket.on("catch-terminate", () => {
+    socket.disconnect();
+    console.log("disconnect");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
